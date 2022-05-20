649bd0b68fc2cf41083070273a56d8345be72111
Tomas Sales <tomas.sales02@gmail.com>
2022-05-20 15:11:48 -0300

    Update simetrica.py

--- Atividades/Matrizes/simetrica.py
+++ Atividades/Matrizes/simetrica.py
@@ -1,21 +1,26 @@
 # Recebe uma matriz e returne True se a matriz for simetrica, em caso contrario retorna False.
-# teste
 
 
-def simetrica(matriz=bool):
+def simetrica(matriz):
 
     print(f"Matriz: {len(matriz), len(matriz[0])}")
 
     for i in range(len(matriz)):
         print("")
+
         for j in range(len(matriz[i])):
-            print(matriz[i][j], "   ", end="")
+            print(matriz[i][j], " ", end="")
+
+    if matriz[0][0] == matriz[1][1] == matriz[2][2] and matriz[0][1] == matriz[1][0] and matriz[0][2] == matriz[2][0] and matriz[1][2] == matriz[2][1]:
+        return True
+    else:
+        return False
 
 
-a = [[11, -3, 4, 8], [-3, 12, 6, 11], [4, 6, 5, 13], [8, 11, 13, 5]]
+a = [[1, 2, 3], [2, 1, 4], [3, 4, 1]]
 
 if simetrica(a):
-    print("Aprovado")
+    print("\n\nAprovado")
 
 else:
-    print("Reprovado")
+    print("\n\nReprovado")
